38f8bee093c8baf462138304295a814e69e95d15
José Wesley Gonçalves <wesleymagnos@hotmail.com>
2023-07-08 15:58:12 -0300

    update factory and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();;
-            $table->string('photo')->nullable();;
             $table->rememberToken();
             $table->timestamps();
         });
